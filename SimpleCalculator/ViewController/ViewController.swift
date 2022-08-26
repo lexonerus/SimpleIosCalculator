5ab63b98f305c3ab143f70abba2d6af1672bf9bb
Alexey Krzywicki <lexone01@gmail.com>
2022-08-26 13:21:15 +0300

    Detach logic from ViewController to CalcService

--- SimpleCalculator/ViewController/ViewController.swift
+++ SimpleCalculator/ViewController/ViewController.swift
@@ -13,12 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UITextField!
     
     // MARK: Properties
-    private var currentOperation = Operations.noAction
-    private var currentNumber    = "0"
-    private var firstNumber      = 0.0
-    private var secondNumber     = 0.0
-    private var isDouble         = false
-    private var result           = ""
+    var service: CalcService?
     
     // MARK: Config appearance
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -30,146 +25,51 @@ class ViewController: UIViewController {
     // MARK: ViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        display.text = currentNumber
+        service = CalcService(displayView: self)
+        display.text = "0"
     }
-}
 
-// MARK: Private Methods
-private extension ViewController {
-    func status() {
-        print(
-            """
-            current number = \(currentNumber)
-            first = \(firstNumber)
-            second = \(secondNumber)
-            result = \(result)
-            """
-        )
-    }
-    
     func updateDisplay(text: String) {
         DispatchQueue.main.async {
             self.display.text = text
         }
-    }
-
-    func acAction() {
-        firstNumber = 0.0
-        secondNumber = 0.0
-        currentOperation = .noAction
-        currentNumber = ""
-        result = ""
-        updateDisplay(text: currentNumber)
-        status()
-        updateDisplay(text: "0")
-    }
-    func makeCalculation(operation: Operations) {
-        if currentOperation != .noAction {
-            
-            if currentNumber != "" {
-                secondNumber = Double(currentNumber)!
-                
-                switch operation {
-                case .noAction:
-                    print("No action")
-                case .addition:
-                    result = String(firstNumber + secondNumber)
-                case .substraction:
-                    result = String(firstNumber - secondNumber)
-                case .division:
-                    result = String(firstNumber / secondNumber)
-                case .multiplication:
-                    result = String(firstNumber * secondNumber)
-                }
-                
-                firstNumber = Double(result)!
-                
-                if Double(result)!.truncatingRemainder(dividingBy: 1) == 0 {
-                    result = String(Int(Double(result)!))
-                }
-                currentNumber = result
-                updateDisplay(text: currentNumber)
-                currentOperation = .noAction
-            }
-
-        } else {
-            firstNumber = Double(currentNumber) ?? 0.0
-            currentNumber = ""
-            updateDisplay(text: "")
-            currentOperation = operation
-        }
-        
-        status()
-
     }
 }
 
 // MARK: Private Actions
 private extension ViewController {
     @IBAction func acPressed(_ sender: Any) {
-        acAction()
+        service?.acAction()
     }
     @IBAction func changeSign(_ sender: Any) {
-        var temp = currentNumber
-        if temp.contains("-") {
-            let sign = ["-"]
-            temp = String(temp.filter { !sign.contains(String($0)) })
-            print(temp)
-            updateDisplay(text: temp)
-            currentNumber = temp
-        } else {
-            temp = "-" + currentNumber
-            print(temp)
-            updateDisplay(text: temp)
-            currentNumber = temp
-        }
+        service?.changeSign()
     }
     @IBAction func percent(_ sender: Any) {
-        currentNumber = String(Double(currentNumber)! / 100)
-        updateDisplay(text: currentNumber)
-        result = currentNumber
-        firstNumber = Double(result)!
+        service?.percent()
     }
     @IBAction func division(_ sender: Any) {
-        makeCalculation(operation: .division)
+        service?.division()
     }
     @IBAction func multiplication(_ sender: Any) {
-        makeCalculation(operation: .multiplication)
+        service?.multiplication()
     }
     @IBAction func substraction(_ sender: Any) {
-        if (currentNumber == "0") || (currentNumber == "") {
-            currentNumber = "-"
-            updateDisplay(text: currentNumber)
-        } else {
-            makeCalculation(operation: .substraction)
-        }
+        service?.substraction()
     }
     @IBAction func addition(_ sender: Any) {
-        makeCalculation(operation: .addition)
+        service?.addition()
     }
     @IBAction func result(_ sender: Any) {
-        makeCalculation(operation: currentOperation)
+        service?.makeResult()
     }
     @IBAction func dot(_ sender: Any) {
-        if currentNumber.contains(".") {
-            return
-        } else {
-            currentNumber += "."
-            updateDisplay(text: currentNumber)
-        }
+        service?.dot()
     }
     @IBAction func swipeLeftOnDisplay(_ sender: Any) {
-        currentNumber.removeLast()
-        updateDisplay(text: currentNumber)
+        service?.removeCharacter()
     }
     @IBAction func numberAction(_ sender: UIButton) {
-        if currentNumber != "0" {
-            currentNumber.append(String(sender.tag))
-            updateDisplay(text: currentNumber)
-        } else {
-            currentNumber = String(sender.tag)
-            updateDisplay(text: currentNumber)
-        }
+        service?.numberAction(number: sender.tag)
     }
 }
 

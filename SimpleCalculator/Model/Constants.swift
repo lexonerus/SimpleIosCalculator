33382aafbd979fea39df034efc6913dca863e99f
Alexey Krzywicki <lexone01@gmail.com>
2022-09-02 11:31:26 +0300

    Create buttons and stacks

---
+++ SimpleCalculator/Model/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SimpleCalculator
+//
+//  Created by Alex Krzywicki on 02.09.2022.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let screenRect    = UIScreen.main.bounds
+    static let screenWidth   = screenRect.size.width
+    static let screenHeight  = screenRect.size.height
+}
--- SimpleCalculator/ViewController/ViewController.swift
+++ SimpleCalculator/ViewController/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: Views
+    let display = UITextField()
+    let mainVStack = UIStackView()
     
     // MARK: Properties
     var service: CalcService?
@@ -18,7 +20,6 @@ class ViewController: UIViewController {
           return .portrait
        }
     }
-
     
     
     // MARK: ViewController lifecycle
@@ -26,6 +27,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemRed
         service = CalcService(displayView: self)
+        configureAppearance()
     }
 
     func updateDisplay(text: String) {
@@ -35,6 +37,73 @@ class ViewController: UIViewController {
 
 // MARK: Private Actions
 private extension ViewController {
+    func configureAppearance() {
+        setupMainVStack()
+        setupButtons()
+    }
 
+    func setupMainVStack() {
+        mainVStack.axis = .vertical
+        mainVStack.frame = CGRect(x: 0, y: 60, width: Constants.screenWidth, height: Constants.screenHeight)
+        mainVStack.alignment = .fill
+        mainVStack.distribution = .fill
+        mainVStack.backgroundColor = .systemGreen
+        view.addSubview(mainVStack)
+        
+    }
+    
+    func createHStack(yPosition: Int) -> UIStackView {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.frame = CGRect(x: 0, y: yPosition, width: Int(Constants.screenWidth), height: 80)
+        return stack
+    }
+    func createButton(order: Int, title: String) -> UIButton {
+
+        let button = UIButton(frame: CGRect(x: (0+Int(Constants.screenWidth/4)*order+2), y: 0, width: Int(Constants.screenWidth)/4-2, height: 80))
+
+        button.setTitle(title, for: .normal)
+        button.backgroundColor = .systemBlue
+        
+        return button
+    }
+    
+    func setupButtons() {
+        let line1 = createHStack(yPosition: 0)
+        line1.addSubview(createButton(order: 0, title: "button 1"))
+        line1.addSubview(createButton(order: 1, title: "button 2"))
+        line1.addSubview(createButton(order: 2, title: "button 3"))
+        line1.addSubview(createButton(order: 3, title: "button 4"))
+        mainVStack.addSubview(line1)
+        
+        let line2 = createHStack(yPosition: 82)
+        line2.addSubview(createButton(order: 0, title: "button 5"))
+        line2.addSubview(createButton(order: 1, title: "button 6"))
+        line2.addSubview(createButton(order: 2, title: "button 7"))
+        line2.addSubview(createButton(order: 3, title: "button 8"))
+        mainVStack.addSubview(line2)
+        
+        let line3 = createHStack(yPosition: 164)
+        line3.addSubview(createButton(order: 0, title: "button 9"))
+        line3.addSubview(createButton(order: 1, title: "button 10"))
+        line3.addSubview(createButton(order: 2, title: "button 11"))
+        line3.addSubview(createButton(order: 3, title: "button 12"))
+        mainVStack.addSubview(line3)
+        
+        let line4 = createHStack(yPosition: 246)
+        line4.addSubview(createButton(order: 0, title: "button 9"))
+        line4.addSubview(createButton(order: 1, title: "button 10"))
+        line4.addSubview(createButton(order: 2, title: "button 11"))
+        line4.addSubview(createButton(order: 3, title: "button 12"))
+        mainVStack.addSubview(line4)
+        
+        let line5 = createHStack(yPosition: 328)
+        line5.addSubview(createButton(order: 0, title: "button 13"))
+        line5.addSubview(createButton(order: 1, title: "button 14"))
+        line5.addSubview(createButton(order: 2, title: "button 15"))
+        line5.addSubview(createButton(order: 3, title: "button 16"))
+        mainVStack.addSubview(line5)
+    
+    }
 }
 
